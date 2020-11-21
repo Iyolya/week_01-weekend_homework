efb5e5c7e91aa176a0c27be3372185d3abb2f586
Laura <almasilaura@outlook.com>
2020-11-21 19:48:43 +0000

    completed test 2

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,8 +1,18 @@
 # WRITE YOUR FUNCTIONS HERE
 
 # 1./
-# We are accessing the value "Camelot of Pets" in the dictionary cc_pet_shop 
-# by it's key "name":
+# We are accessing the value "Camelot of Pets" 
+# in the dictionary cc_pet_shop by it's key "name":
+
 def get_pet_shop_name(shop):
     return shop["name"]
-    
+
+# 2./
+#We are accessing the value of the "total_cash" key
+# by accessing the internal dictionary "admin" inside 
+# the nested dictionary 'cc_pet_shop by':
+
+def get_total_cash(shop):
+    return shop["admin"]["total_cash"]
+
+
