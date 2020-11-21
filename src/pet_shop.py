42b0f5231dfa99ae2d67ae4080a2aca6a43c09b8
Laura <almasilaura@outlook.com>
2020-11-21 19:23:56 +0000

    completed test 1

---
+++ src/pet_shop.py
@@ -0,0 +1,8 @@
+# WRITE YOUR FUNCTIONS HERE
+
+# 1./
+# We are accessing the value "Camelot of Pets" in the dictionary cc_pet_shop 
+# by it's key "name":
+def get_pet_shop_name(shop):
+    return shop["name"]
+    
