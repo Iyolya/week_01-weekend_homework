173a118213239343c694e34dde4b932de0698709
Laura <almasilaura@outlook.com>
2020-11-22 04:16:25 +0000

    completed test 18

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -130,3 +130,10 @@ def get_customer_pet_count(customer):
 
 def add_pet_to_customer(customer, new_pet):
     customer["pets"].append(new_pet)
+    
+# 18./ WORKS!!!
+# ....
+def customer_can_afford_pet(customer, new_pet):
+    if customer["cash"] >= new_pet["price"]:
+        return True
+    
